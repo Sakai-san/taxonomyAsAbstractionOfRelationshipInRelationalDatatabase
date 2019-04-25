c5952a3897baa1cd1ca5698b5cbb1da2876a7311
Sakai-san <o_edo@hotmail.com>
2019-04-26 00:06:51 +0200

    Test query with generic schema.

--- schema.sql
+++ schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE `Meta` (
   `table_name` bigint(20) UNSIGNED NOT NULL,  
   `meta_key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `meta_value` longtext COLLATE utf8mb4_unicode_ci,
-  PRIMARY KEY (meta_id),
-  UNIQUE KEY (foreign_table_id, table_name, meta_key)
+  PRIMARY KEY (meta_id)
+--  UNIQUE KEY (foreign_table_id, table_name, meta_key)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -22,7 +22,7 @@ CREATE TABLE `Relationship` (
   `content_id` bigint(20) UNSIGNED NOT NULL DEFAULT '0',
   `taxonomy_id` bigint(20) UNSIGNED NOT NULL DEFAULT '0',
   `term_order` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (content_id, term_taxonomy_id)
+  PRIMARY KEY (content_id, taxonomy_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -76,21 +76,48 @@ INSERT INTO `Term` (`term_id`, `name`) VALUES
 
 
 INSERT INTO `Taxonomy` (`taxonomy_id`, `term_id`, `taxonomy`, `parent`) VALUES
-(1, (SELECT term_id FROM Term WHERE name = 'FRA'), 'Internationalization' ),
-(2, (SELECT term_id FROM Term WHERE name = 'FRA'), 'Internationalization' ),
-(3, (SELECT term_id FROM Term WHERE name = 'ENG'), 'Internationalization' ),
-(4, (SELECT term_id FROM Term WHERE name = 'Male'), 'Person' ),
-(5, (SELECT term_id FROM Term WHERE name = 'Female'), 'Person' ),
-(6, (SELECT term_id FROM Term WHERE name = 'DEU'), 'Language' ),
-(7, (SELECT term_id FROM Term WHERE name = 'FRA'), 'Language' ),
-(8, (SELECT term_id FROM Term WHERE name = 'IT'), 'Employee', (SELECT taxonomy_id FROM Taxonomy WHERE taxonomy = 'Person' ) ),
-(9, (SELECT term_id FROM Term WHERE name = 'Editing'), 'Employee', (SELECT taxonomy_id FROM Taxonomy WHERE taxonomy = 'Person' ) ),
-(10, (SELECT term_id FROM Term WHERE name = 'SQL'), 'Course' ),
-(11, (SELECT term_id FROM Term WHERE name = 'Theory'), 'Course', (SELECT taxonomy_id FROM Taxonomy WHERE taxonomy = 'Course' AND term_id = (SELECT term_id FROM Term WHERE name = 'SQL') ) ),
-(12, (SELECT term_id FROM Term WHERE name = 'Chapter'), 'Course', (SELECT taxonomy_id FROM Taxonomy WHERE taxonomy = 'Course' AND term_id = (SELECT term_id FROM Term WHERE name = 'Theory') ) );
+(1, (SELECT term_id FROM Term WHERE name = 'FRA'), 'Internationalization', 0 ),
+(2, (SELECT term_id FROM Term WHERE name = 'FRA'), 'Internationalization', 0 ),
+(3, (SELECT term_id FROM Term WHERE name = 'ENG'), 'Internationalization', 0 ),
+(4, (SELECT term_id FROM Term WHERE name = 'Male'), 'Person', 0 ), 
+(5, (SELECT term_id FROM Term WHERE name = 'Female'), 'Person', 0 ), 
+(6, (SELECT term_id FROM Term WHERE name = 'DEU'), 'Language', 0 ),
+(7, (SELECT term_id FROM Term WHERE name = 'FRA'), 'Language', 0 ),
+(8, (SELECT term_id FROM Term WHERE name = 'IT'), 'Employee', 0 ),
+(9, (SELECT term_id FROM Term WHERE name = 'Editing'), 'Employee', 0 ),
+(10, (SELECT term_id FROM Term WHERE name = 'SQL'), 'Course', 0 ),
+(11, (SELECT term_id FROM Term WHERE name = 'Theory'), 'Course', 10 ),
+(12, (SELECT term_id FROM Term WHERE name = 'Practice'), 'Course', 10 ),
+(13, (SELECT term_id FROM Term WHERE name = 'Chapter'), 'Course', 11 ),
+(14, (SELECT term_id FROM Term WHERE name = 'Chapter'), 'Course', 12 );
 
 
 INSERT INTO `Relationship` (`content_id`, `taxonomy_id`, `term_order`) VALUES
-( (SELECT content_id FROM Content WHERE title = 'Apprendre SQL'), (SELECT taxonomy_id FROM Taxonomy WHERE taxonomy = 'Course' AND term_id = (SELECT term_id FROM Term WHERE name='SQL' ) ) ),
+( (SELECT content_id FROM Content WHERE title = 'Apprendre SQL'), (SELECT taxonomy_id FROM Taxonomy WHERE taxonomy = 'Course' AND term_id = (SELECT term_id FROM Term WHERE name='SQL' ) ), 0 ),
 ( (SELECT content_id FROM Content WHERE title = 'Chapitre 1 : Introduction'), (SELECT taxonomy_id FROM Taxonomy WHERE taxonomy = 'Course' AND term_id = (SELECT term_id FROM Term WHERE name='Chapter' ) ), 1 );
 
+
+/*
+SELECT *
+FROM Content AS C
+INNER JOIN Relationship AS R ON C.content_id = R.content_id
+INNER JOIN Taxonomy AS TA ON TA.taxonomy_id = R.taxonomy_id
+WHERE TA.taxonomy_id = (
+	select taxonomy_id
+    from Taxonomy AS TA
+    where TA.taxonomy_id = (
+    	select taxonomy_id
+	    from Taxonomy AS TA3
+    	where TA3.term_id = (select term_id from Term where name= 'Chapter' ) AND TA3.parent = (
+        	select taxonomy_id
+    		from Taxonomy AS TA2
+    		where TA2.term_id = (select term_id from Term where name='Theory') AND TA2.parent = (
+	            select taxonomy_id
+    			from Taxonomy AS TA1
+    			where TA1.taxonomy = 'Course' and TA1.term_id = (select term_id from Term where name = 'SQL' )
+            )
+        )
+    )
+)
+
+*/
